2be3819cf187c5eb15f7a7660290990d7fce8216
ypranab <ypranab11@gmail.com>
2022-12-05 23:29:58 +0600

    vercel prod for course url

--- index.js
+++ index.js
@@ -5,10 +5,41 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const catagories = require('./data/catagories.json');
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
-    res.send('eLearning API is Running');
+    res.send('Learning API is Running');
 });
 
-app.listen(port, () => {
-    console.log("Lerning platform is running");
+app.get('/course-catagories', (req, res) => {
+    res.send(catagories)
+});
+
+app.get('/course/:id',(req,res)=>{
+  const id = req.params.id;
+  const selectedCourse = courses.find(item => item._id === id);
+  res.send(selectedCourse);
 })
+
+app.get('/course',(req,res)=>{
+	res.send(courses);
+})
+
+app.get('/catagory/:id',(req,res)=>{
+  const id = req.params.id;
+  if(id==='08'){
+    res.send(courses);
+  }
+  else{
+    const catagoryCourses = news.filter(item => item.catagory_id === id);
+    res.send(catagoryCourses);
+  }
+})
+
+app.listen(port, () => {
+    console.log("Learning platform is running");
+})
+
+//https://learning-school-server-side.vercel.app/
+//vercel --prod
